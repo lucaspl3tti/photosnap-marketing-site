5ccc7d56320cb0aa4c4eacf4080cdb4d48cec359
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2022-03-07 21:29:14 +0100

    Add website data to project

---
+++ src/stores/data.js
@@ -0,0 +1,302 @@
+export const websiteData = {
+    'navigation': {
+        'logo': './images/shared/logo.svg',
+        'navItems': {
+            'home': {
+                'title': 'Home',
+                'href': '/',
+            },
+            'stories': {
+                'title': 'Stories',
+                'href': '/stories',
+            },
+            'features': {
+                'title': 'Features',
+                'href': '/features',
+            },
+            'pricing': {
+                'title': 'Pricing',
+                'href': '/pricing',
+            },
+        },
+        'inviteBtn': 'Get an invite',
+    },
+    'homepage': {
+        'introduction': {
+            'heading': 'Create and share your photo Stories',
+            'copy': 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
+            'link': 'Get an invite',
+            'image': './images/home/desktop/create-and-share.jpg',
+            'class': 'introduction',
+        },
+        'storyTemplates': {
+            'heading': 'Beautiful stories every time',
+            'copy': 'We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.',
+            'link': 'View the stories',
+            'image': './images/home/desktop/beautiful-stories.jpg',
+            'class': 'templates',
+        },
+        'audience': {
+            'heading': 'Designed for everyone',
+            'copy': 'Photosnap can help you create stories that resonate with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it.',
+            'link': 'View the stories',
+            'image': './images/home/desktop/designed-for-everyone.jpg',
+            'class': 'audience',
+        },
+    },
+    'storiesPage': {
+        'header': {
+            'subline': 'Last month\'s featured story',
+            'headline': 'Hazy full moon of Appalachia',
+            'date': 'March 2nd 2020',
+            'publishedBy': 'John Appleseed',
+            'description': 'The dissected plateau area, while not actually made up of geological mountains, is popularly called "mountains," especially in eastern Kentucky and West Virginia, and while the ridges are not high, the terrain is extremely rugged.',
+            'image': '',
+        },
+        'stories': {
+            1: {
+                'date': 'April 16th 2020',
+                'headline': 'The Mountains',
+                'publishedBy': 'John Appleseed',
+                'image': '',
+            },
+            2: {
+                'date': 'April 14th 2020',
+                'headline': 'Sunset Cityscapes',
+                'publishedBy': 'Benjamin Cruz',
+                'image': '',
+            },
+            3: {
+                'date': 'April 11th 2020',
+                'headline': '18 Days Voyage',
+                'publishedBy': 'Alexei Borodin',
+                'image': '',
+            },
+            4: {
+                'date': 'April 9th 2020',
+                'headline': 'Architecturals',
+                'publishedBy': 'Samantha Brooke',
+                'image': '',
+            },
+            5: {
+                'date': 'April 7th 2020',
+                'headline': 'World Tour 2019',
+                'publishedBy': 'Timothy Wagner',
+                'image': '',
+            },
+            6: {
+                'date': 'April 3rd 2020',
+                'headline': 'Unforeseen Corners',
+                'publishedBy': 'William Malcolm',
+                'image': '',
+            },
+            7: {
+                'date': 'March 29th 2020',
+                'headline': 'King on Africa: Part II',
+                'publishedBy': 'Tim Hillenburg',
+                'image': '',
+            },
+            8: {
+                'date': 'March 21st 2020',
+                'headline': 'The Trip to Nowhere',
+                'publishedBy': 'Felicia Rourke',
+                'image': '',
+            },
+            9: {
+                'date': 'March 19th 2020',
+                'headline': 'Rage of The Sea',
+                'publishedBy': 'Mohammed Abdul',
+                'image': '',
+            },
+            10: {
+                'date': 'March 16th 2020',
+                'headline': 'Running Free',
+                'publishedBy': 'Michelle',
+                'image': '',
+            },
+            11: {
+                'date': 'March 11th 2020',
+                'headline': 'Behind the Waves',
+                'publishedBy': 'Lamarr Wilson',
+                'image': '',
+            },
+            12: {
+                'date': 'March 9th 2020',
+                'headline': 'Calm Waters',
+                'publishedBy': 'Samantha Brooke',
+                'image': '',
+            },
+            13: {
+                'date': 'March 5th 2020',
+                'headline': 'The Milky Way',
+                'publishedBy': 'Benjamin Cruz',
+                'image': '',
+            },
+            14: {
+                'date': 'March 4th 2020',
+                'headline': 'Night at The Dark Forest',
+                'publishedBy': 'Mohammed Abdul',
+                'image': '',
+            },
+            15: {
+                'date': 'March 1st 2020',
+                'headline': 'Somwarpet\'s Beauty',
+                'publishedBy': 'Michelle',
+                'image': '',
+            },
+            16: {
+                'date': 'February 25th 2020',
+                'headline': 'Land of Dreams',
+                'publishedBy': 'William Malcolm',
+                'image': '',
+            },
+        },
+    },
+    'featuresPage': {
+        'header': {
+            'headline': '',
+            'copy': '',
+            'image': '',
+        },
+        'features': {
+            1: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+            2: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+            3: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+            4: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+            5: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+            6: {
+                'icon': '',
+                'heading': '',
+                'copy': '',
+            },
+        },
+    },
+    'pricingPage': {
+        'header': {
+            'headline': '',
+            'copy': '',
+            'image': '',
+        },
+        'memberships': {
+            'basic': {
+                'title': '',
+                'description': '',
+                'priceMonthly': '',
+                'priceYearly': '',
+            },
+            'pro': {
+                'title': '',
+                'description': '',
+                'priceMonthly': '',
+                'priceYearly': '',
+            },
+            'business': {
+                'title': '',
+                'description': '',
+                'priceMonthly': '',
+                'priceYearly': '',
+            },
+        },
+        'comparison': {
+            'features': {
+                'unlimitedStoryPosting': {
+                    'title': 'Unlimited Story Posting',
+                    'basic': true,
+                    'pro': true,
+                    'business': true,
+                },
+                'unlimitedPhotoUpload': {
+                    'title': 'Unlimited Photo Upload',
+                    'basic': true,
+                    'pro': true,
+                    'business': true,
+                },
+                'customContent': {
+                    'title': 'Embedding custom content',
+                    'basic': false,
+                    'pro': true,
+                    'business': true,
+                },
+                'customizeMetaData': {
+                    'title': 'Customize Meta Data',
+                    'basic': false,
+                    'pro': true,
+                    'business': true,
+                },
+                'advancedMetrics': {
+                    'title': 'Advanced Metrics',
+                    'basic': false,
+                    'pro': false,
+                    'business': true,
+                },
+                'downloads': {
+                    'title': 'Photo Downloads',
+                    'basic': false,
+                    'pro': false,
+                    'business': true,
+                },
+                'seo': {
+                    'title': 'Search Engine Index',
+                    'basic': false,
+                    'pro': false,
+                    'business': true,
+                },
+                'customAnalytics': {
+                    'title': 'Custom Analytics',
+                    'basic': false,
+                    'pro': false,
+                    'business': true,
+                },
+            }
+        },
+    },
+    'beta': {
+        'title': 'We\'re in beta. Get your invite today!',
+        'link': 'Get an invite',
+    },
+    'footer': {
+        'socialIcons': {
+            'facebook': {
+                'icon': '',
+                'href': '',
+            },
+            'youtube': {
+                'icon': '',
+                'href': '',
+            },
+            'twitter': {
+                'icon': '',
+                'href': '',
+            },
+            'pinterest': {
+                'icon': '',
+                'href': '',
+            },
+            'instagram': {
+                'icon': '',
+                'href': '',
+            },
+        },
+        'copyright': 'Copyright 2022. All Rights Reserved',
+    },
+}
